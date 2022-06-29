0788f6d4c4eeb0152cf775303e7935aeef41e69f
saffiullahfahim <fahim14@student.sust.edu>
2022-06-30 01:16:50 +0600

    init

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+import d from "./NTechDOM.js";
+import { formDiv } from "../../modules/form.js";
+
+d.render("root", formDiv);
---
+++ modules/form.js
@@ -0,0 +1,5 @@
+import d from "../assets/js/NTechDOM.js";
+
+const formDiv = d.createElement("div");
+
+export { formDiv };
